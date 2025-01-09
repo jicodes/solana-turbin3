cb5e98fdd4af38d771f14beda95ea074e45a799d
jicodes <122512755+jicodes@users.noreply.github.com>
2025-01-09 12:57:54 +0800

    3. transfer SOL

--- enrollment-rust/src/lib.rs
+++ enrollment-rust/src/lib.rs
@@ -3,12 +3,17 @@ mod tests {
     use solana_sdk::{
         pubkey::Pubkey,
         signature::{read_keypair_file, Keypair, Signer},
+        transaction::Transaction,
     };
 
     use bs58;
     use std::io::{self, BufRead};
 
     use solana_client::rpc_client::RpcClient;
+
+    use solana_program::system_instruction::transfer;
+
+    use std::str::FromStr;
 
     const RPC_URL: &str = "https://api.devnet.solana.com";
 
@@ -69,7 +74,41 @@ mod tests {
             Err(e) => println!("Oops, something went wrong: {}", e),
         };
     }
-    
+
     #[test]
-    fn transfer_sol() {}
+    fn transfer_sol() {
+        // Import our keypair
+        let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
+        // Define our Turbin3 public key
+        let to_pubkey = Pubkey::from_str("<your Turbin3 public key>").unwrap();
+
+        // Create a Solana devnet connection
+        let rpc_client = RpcClient::new(RPC_URL);
+
+        // In order to sign transactions, we're going to need to get a recent blockhash,
+        // as signatures are designed to expire as a security feature:
+        // Get recent blockhash
+        let recent_blockhash = rpc_client
+            .get_latest_blockhash()
+            .expect("Failed to get recent blockhash");
+
+        // Create a new transaction
+        let transaction = Transaction::new_signed_with_payer(
+            &[transfer(&keypair.pubkey(), &to_pubkey, 1_000_000)],
+            Some(&keypair.pubkey()),
+            &vec![&keypair],
+            recent_blockhash,
+        );
+
+        // Send the transaction
+        let signature = rpc_client
+            .send_and_confirm_transaction(&transaction)
+            .expect("Failed to send transaction");
+
+        // Print our transaction out
+        println!(
+            "Success! Check out your TX here: https://explorer.solana.com/tx/{}/?cluster=devnet",
+            signature
+        );
+    }
 }
