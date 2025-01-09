c5f391425610410901f7fe4b3a91376dabc4897a
jicodes <122512755+jicodes@users.noreply.github.com>
2025-01-09 11:29:28 +0800

    init rust project

---
+++ enrollment-rust/target/debug/deps/enrollment_rust-88d84e304f7d8b89.d
@@ -0,0 +1,9 @@
+/Users/d/web3/solana/solana-turbin3/enrollment-rust/target/debug/deps/libenrollment_rust-88d84e304f7d8b89.rmeta: src/lib.rs Cargo.toml
+
+/Users/d/web3/solana/solana-turbin3/enrollment-rust/target/debug/deps/enrollment_rust-88d84e304f7d8b89.d: src/lib.rs Cargo.toml
+
+src/lib.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ enrollment-rust/target/debug/deps/enrollment_rust-b233b765daadc9ae.d
@@ -0,0 +1,9 @@
+/Users/d/web3/solana/solana-turbin3/enrollment-rust/target/debug/deps/libenrollment_rust-b233b765daadc9ae.rmeta: src/lib.rs Cargo.toml
+
+/Users/d/web3/solana/solana-turbin3/enrollment-rust/target/debug/deps/enrollment_rust-b233b765daadc9ae.d: src/lib.rs Cargo.toml
+
+src/lib.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
