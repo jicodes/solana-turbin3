07a6415446bb7fcdb7725e68bbc0e18249d7a2ca
jicodes <122512755+jicodes@users.noreply.github.com>
2025-04-04 10:05:50 +0800

    feat: mint NFT on devnet with generated metadata uri

--- 01-solana-starter/ts/cluster1/nft_mint.ts
+++ 01-solana-starter/ts/cluster1/nft_mint.ts
@@ -2,7 +2,7 @@ import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi"
 import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 
-import wallet from "../wba-wallet.json"
+import wallet from "../dev-wallet.json"
 import base58 from "bs58";
 
 const RPC_ENDPOINT = "https://api.devnet.solana.com";
@@ -14,13 +14,25 @@ umi.use(signerIdentity(myKeypairSigner));
 umi.use(mplTokenMetadata())
 
 const mint = generateSigner(umi);
+const metadata_uri = "https://devnet.irys.xyz/C8RMsKS1zs8b4UPuotDcfZpyWqFrucTV9nUD7fM9tixQ";
 
 (async () => {
-    // let tx = ???
-    // let result = await tx.sendAndConfirm(umi);
-    // const signature = base58.encode(result.signature);
+    let tx = createNft(umi, {
+        mint,
+        name: "Rug Day NFT",
+        symbol: "RUG",
+        uri: metadata_uri, 
+        sellerFeeBasisPoints: percentAmount(0), // % royalty
+        creators: [{
+            address: keypair.publicKey,
+            verified: true,
+            share: 100
+        }]
+    });
+    let result = await tx.sendAndConfirm(umi);
+    const signature = base58.encode(result.signature);
     
-    // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+    console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
 
     console.log("Mint Address: ", mint.publicKey);
 })();
