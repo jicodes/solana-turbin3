368c9a027424df2597889699411a5efa0d277565
jicodes <122512755+jicodes@users.noreply.github.com>
2025-04-04 09:58:29 +0800

    feat: upload nft metadata using irys

--- 01-solana-starter/ts/cluster1/nft_metadata.ts
+++ 01-solana-starter/ts/cluster1/nft_metadata.ts
@@ -1,4 +1,4 @@
-import wallet from "../wba-wallet.json"
+import wallet from "../dev-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
@@ -17,27 +17,31 @@ umi.use(signerIdentity(signer));
         // Follow this JSON structure
         // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
 
-        // const image = ???
-        // const metadata = {
-        //     name: "?",
-        //     symbol: "?",
-        //     description: "?",
-        //     image: "?",
-        //     attributes: [
-        //         {trait_type: '?', value: '?'}
-        //     ],
-        //     properties: {
-        //         files: [
-        //             {
-        //                 type: "image/png",
-        //                 uri: "?"
-        //             },
-        //         ]
-        //     },
-        //     creators: []
-        // };
-        // const myUri = ???
-        // console.log("Your metadata URI: ", myUri);
+        // NFT metadata
+        const imageUri = "https://devnet.irys.xyz/DZESpEdnAino4zZfGzaxUkdVkKZPmNzvT9Uo9ZeREzB8";
+        const metadata = {
+            name: "Rug Day NFT",
+            symbol: "RUG",
+            description: "A special NFT celebrating Rug Day",
+            image: imageUri,
+            attributes: [
+                {trait_type: 'Cohort', value: 'Rug day'}
+            ],
+            properties: {
+                files: [
+                    {
+                        type: "image/jpeg",
+                        uri: imageUri
+                    },
+                ]
+            },
+            creators: []
+        };
+        // Convert metadata to JSON and upload
+        const metadataBuffer = Buffer.from(JSON.stringify(metadata));
+        const metadataFile = createGenericFile(metadataBuffer, "metadata.json", { contentType: "application/json" });
+        const [metadataUri] = await umi.uploader.upload([metadataFile]);
+        console.log("Your metadata URI: ", metadataUri);
     }
     catch(error) {
         console.log("Oops.. Something went wrong", error);
