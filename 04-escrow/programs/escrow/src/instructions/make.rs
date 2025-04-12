a6ff2d17a98e4e6aaa1c370fc58b5c9dac09ab60
jicodes <122512755+jicodes@users.noreply.github.com>
2025-04-12 20:08:21 +0800

    feat: implement deposit and save escrow functions

--- 04-escrow/programs/escrow/src/instructions/make.rs
+++ 04-escrow/programs/escrow/src/instructions/make.rs
@@ -2,7 +2,7 @@ use anchor_lang::prelude::*;
 
 use anchor_spl::{
     associated_token::AssociatedToken,
-    token_interface::{Mint, TokenAccount, TokenInterface},
+    token_interface::{transfer_checked, Mint, TokenAccount, TokenInterface, TransferChecked},
 };
 
 use crate::Escrow;
@@ -46,4 +46,32 @@ pub struct Make<'info> {
     pub associated_token_program: Program<'info, AssociatedToken>,
     pub token_program: Interface<'info, TokenInterface>,
     pub system_program: Program<'info, System>,
+}
+
+impl Make<'_> {
+    pub fn deposit(&mut self, deposit: u64) -> Result<()> {
+        let transfer_accounts = TransferChecked {
+            from: self.maker_ata_a.to_account_info(),
+            mint: self.mint_a.to_account_info(),
+            to: self.vault.to_account_info(),
+            authority: self.maker.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new(self.token_program.to_account_info(), transfer_accounts);
+
+        transfer_checked(cpi_ctx, deposit, self.mint_a.decimals)
+    }
+
+    pub fn save_escrow(&mut self, seed: u64, bumps: &MakeBumps, receive: u64) -> Result<()> {
+        self.escrow.set_inner(Escrow {
+            seed,
+            bump: bumps.escrow,
+            maker: self.maker.key(),
+            mint_a: self.mint_a.key(),
+            mint_b: self.mint_b.key(),
+            receive,
+        });
+
+        Ok(())
+    }
 }
--- 04-escrow/programs/escrow/src/lib.rs
+++ 04-escrow/programs/escrow/src/lib.rs
@@ -3,15 +3,18 @@ use anchor_lang::prelude::*;
 pub mod state;
 pub use state::*;
 
+pub mod instructions;
+use instructions::*;
+
 declare_id!("77tyVbm78iJk3ktLYiWyCM6JBjevChhGeSxSGwceDA3Z");
 
 #[program]
 pub mod escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn make(ctx: Context<Make>, seed: u64, deposit: u64, receive: u64) -> Result<()> {
+        ctx.accounts.deposit(deposit)?;
+        ctx.accounts.save_escrow(seed, &ctx.bumps, receive)
     }
 }
 
