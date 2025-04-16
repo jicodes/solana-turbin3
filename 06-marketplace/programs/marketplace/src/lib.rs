e3c4fca9ce0336d0e650975de3b43a9bd58e4b3a
jicodes <122512755+jicodes@users.noreply.github.com>
2025-04-16 11:40:32 +0800

    add NFT marketplace project

---
+++ 06-marketplace/programs/marketplace/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("77Nqr3YcueeRW3aVFziBEHnYcx6ZrerP6FNdJvHc2bU1");
+
+#[program]
+pub mod marketplace {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
