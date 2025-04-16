9301550bfae03b9a7b0303aaa6ef7bada25c8328
jicodes <122512755+jicodes@users.noreply.github.com>
2025-04-16 20:56:11 +0800

    fix: format

--- 06-marketplace/programs/marketplace/src/instructions/list.rs
+++ 06-marketplace/programs/marketplace/src/instructions/list.rs
@@ -13,64 +13,64 @@ pub struct List<'info> {
     #[account(mut)]
     pub maker: Signer<'info>,
     #[account(
-    seeds = [b"marketplace", marketplace.name.as_bytes()],
-    bump = marketplace.bump,
-  )]
+      seeds = [b"marketplace", marketplace.name.as_bytes()],
+      bump = marketplace.bump,
+    )]
     pub marketplace: Account<'info, Marketplace>,
 
     pub maker_mint: InterfaceAccount<'info, Mint>,
     #[account(
-    mut,
-    associated_token::mint = maker_mint,
-    associated_token::authority = maker,
-  )]
+      mut,
+      associated_token::mint = maker_mint,
+      associated_token::authority = maker,
+    )]
     pub maker_ata: InterfaceAccount<'info, TokenAccount>,
     #[account(
-    init,
-    payer = maker,
-    associated_token::mint = maker_mint,
-    associated_token::authority = listing,
-  )]
+      init,
+      payer = maker,
+      associated_token::mint = maker_mint,
+      associated_token::authority = listing,
+    )]
     pub vault: InterfaceAccount<'info, TokenAccount>,
     #[account(
-    init,
-    payer = maker,
-    seeds = [b"listing", marketplace.key().as_ref(), maker_mint.key().as_ref()],
-    bump,
-    space = Listing::INIT_SPACE
-  )]
+      init,
+      payer = maker,
+      seeds = [b"listing", marketplace.key().as_ref(), maker_mint.key().as_ref()],
+      bump,
+      space = Listing::INIT_SPACE
+    )]
     pub listing: Account<'info, Listing>,
 
     pub collection_mint: InterfaceAccount<'info, Mint>,
     #[account(
-    seeds = [
-      b"metadata", 
-      metadata_program.key().as_ref(),
-      maker_mint.key().as_ref(),
-    ],
-    seeds::program = metadata_program.key(),
-    bump,
-    constraint = metadata.collection.as_ref().unwrap().key.as_ref() == collection_mint.key().as_ref(),
-    constraint = metadata.collection.as_ref().unwrap().verified,
-  )]
+      seeds = [
+        b"metadata", 
+        metadata_program.key().as_ref(),
+        maker_mint.key().as_ref(),
+      ],
+      seeds::program = metadata_program.key(),
+      bump,
+      constraint = metadata.collection.as_ref().unwrap().key.as_ref() == collection_mint.key().as_ref(),
+      constraint = metadata.collection.as_ref().unwrap().verified,
+    )]
     pub metadata: Account<'info, MetadataAccount>,
 
     #[account(
-    seeds = [
-      b"metadata", 
-      metadata_program.key().as_ref(),
-      maker_mint.key().as_ref(),
-      b"edition",
-    ],
-    seeds::program = metadata_program.key(),
-    bump,
-  )]
+      seeds = [
+        b"metadata", 
+        metadata_program.key().as_ref(),
+        maker_mint.key().as_ref(),
+        b"edition",
+      ],
+      seeds::program = metadata_program.key(),
+      bump,
+    )]
     pub master_edition: Account<'info, MasterEditionAccount>,
 
     pub metadata_program: Program<'info, Metadata>, // Metaplex program
     pub associated_token_program: Program<'info, AssociatedToken>, // for creating associated token accounts
     pub system_program: Program<'info, System>,                    // for creating accounts
-    pub token_program: Interface<'info, TokenInterface>,             // for token operations
+    pub token_program: Interface<'info, TokenInterface>,           // for token operations
 }
 
 impl List<'_> {
