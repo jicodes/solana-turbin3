8a642a054cbdd582d3ac89988dc28fec75f27938
jicodes <122512755+jicodes@users.noreply.github.com>
2025-04-21 23:19:44 +0800

    feat: add instruction implementations to the program

--- 05-amm/programs/amm/src/lib.rs
+++ 05-amm/programs/amm/src/lib.rs
@@ -15,7 +15,16 @@ declare_id!("3kANf1LyNTbW6z5hZgFjqVNifw1mS49As7Y93bD7QqZv");
 pub mod amm {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        seed: u64,
+        fee: u16,
+        authority: Option<Pubkey>,
+    ) -> Result<()> {
+        ctx.accounts.initialize(seed, fee, authority, &ctx.bumps)
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, lp_amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+        ctx.accounts.deposit(lp_amount, max_x, max_y)
     }
 }
