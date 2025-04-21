4d1e40d7520b171d75618c3eabb5d16d60b1ed5f
jicodes <122512755+jicodes@users.noreply.github.com>
2025-04-21 21:33:40 +0800

    feat: implement initialize instruction for AMM

--- 05-amm/programs/amm/src/state/mod.rs
+++ 05-amm/programs/amm/src/state/mod.rs
@@ -4,8 +4,8 @@ use anchor_lang::prelude::*;
 pub struct Config {
     pub seed: u64,
     pub authority: Option<Pubkey>,
-    pub mint_a: Pubkey,
-    pub mint_b: Pubkey,
+    pub mint_x: Pubkey,
+    pub mint_y: Pubkey,
     pub fee: u16,
     pub locked: bool,
     pub config_bump: u8,
